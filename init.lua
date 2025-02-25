25c60a083e6671adee774b0802ef607f649ae101
Riky47 <56732510+Riky47@users.noreply.github.com>
2025-02-25 22:10:32 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,2 +1,2 @@
-local loader = loadstring("https://raw.githubusercontent.com/SPDM-Team/Arceus-X-NEO-public/refs/heads/main/plugin-loader")()
+local loader = loadstring(game:HttpGet("https://raw.githubusercontent.com/SPDM-Team/Arceus-X-NEO-public/refs/heads/main/plugin-loader"))()
 loader:Load(game:HttpGet("https://raw.githubusercontent.com/AXScripts/AXHub/refs/heads/main/main.lua"))
